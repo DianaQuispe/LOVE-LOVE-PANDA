6e19cf8d5f581fed6409df1da6695704c8613875
MyDiana <danaalienmas@gmail.com>
2017-08-09 07:21:58 -0500

    imagenes puestas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
 
-var section = document.getElementById('pictures');
+var section = document.getElementById('imagenes');
 for (var i = 0; i < pictures.length; i++) {
-  var photo = document.createElement('img');
-  photo.setAttribute('src', pictures[i]);
-  photo.setAttribute('class', 'photo');
-  section.appendChild(photo);
+  var pandas = document.createElement('img');
+  pandas.setAttribute('src', pictures[i]);
+  pandas.setAttribute('class', 'pandas');
+  section.appendChild(pandas);
 }
