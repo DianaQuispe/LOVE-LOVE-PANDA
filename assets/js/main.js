bcb27720788e57d38e88f663c1b95f56ce1af573
DianaQuispe <danaalienmas@gmail.com>
2017-08-09 12:02:57 -0500

    Trabajo finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
-
 var section = document.getElementById('imagenes');
 for (var i = 0; i < pictures.length; i++) {
   var pandas = document.createElement('img');
